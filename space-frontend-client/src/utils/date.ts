3af345b9282e2c8e9a91151749a0c644af58f02f
Pablo Y. Prado <93798700+pyurips@users.noreply.github.com>
2025-05-09 10:34:48 -0300

    Conserto relacionado as datas dos pedidos (#14)

--- space-frontend-client/src/utils/date.ts
+++ space-frontend-client/src/utils/date.ts
@@ -2,12 +2,12 @@ import moment from "moment";
 
 export const formatDate = (dateString: string): string => {
   moment.locale("pt-br");
-  return moment(dateString).format("DD/MM/YYYY");
+  return moment.utc(dateString).format("DD/MM/YYYY");
 };
 
 export const formatDateTime = (dateString: string): string => {
   moment.locale("pt-br");
-  return moment(dateString).format("DD/MM/YYYY HH:mm");
+  return moment.utc(dateString).format("DD/MM/YYYY HH:mm");
 };
 
 export const getRelativeTime = (dateString: string): string => {
